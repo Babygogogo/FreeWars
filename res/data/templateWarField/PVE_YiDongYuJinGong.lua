0fc2081c200feeda94e034d28c1a39ebfb578e19
Babygogogo <101599838@qq.com>
2017-04-12 12:38:42 +0800

    [Add] Enable advanced settings for campaign maps.

--- res/data/templateWarField/PVE_YiDongYuJinGong.lua
+++ res/data/templateWarField/PVE_YiDongYuJinGong.lua
@@ -6,6 +6,21 @@ return {
   width = 12,
   height = 9,
 
+  advancedSettings = {
+    attackModifier            = 0,     -- 全局攻击力加成（百分比）：0=无加成（默认），-30=原作沙尘暴效果。可用范围：-100 ~ 无穷大
+    energyGainModifier        = 100,   -- 全局能量获取速度倍率（百分比）：100=默认速率（默认），0=无法获得能量，200=两倍获取速度。可用范围：0 ~ 无穷大
+    incomeModifier            = 100,   -- 全局收入倍率（百分比）：100=1000收入（默认），0=无收入，200=两倍（即2000）收入。可用范围：0 ~ 无穷大
+    isActiveSkillEnabled      = true,  -- 是否启用主动技（布尔值）：true=是（默认），false=否。
+    isFogOfWarByDefault       = false, -- 是否雾战（布尔值）：true=是，false=否（默认）。
+    isPassiveSkillEnabled     = true,  -- 是否启用日常技（布尔值）：true=是（默认），false=否。
+    isSkillDeclarationEnabled = true,  -- 是否启用主动技宣言（布尔值）：true=是（默认），false=否。
+    playerIndex               = 1,     -- 玩家行动顺序（正整数）：1=红方（默认），2=蓝，3=黄，4=黑。可用范围：1 ~ 地图玩家总数
+    moveRangeModifier         = 0,     -- 全局部队移动力加成（整数）：0=无加成（默认），-1=全部减一，1=全部+1。可用范围：任意整数（无论如何，部队最低移动力为1）
+    startingEnergy            = 0,     -- 全局初始能量值（整数）：0=0（默认）。可用范围：0 ~ 无穷大
+    startingFund              = 0,     -- 全局初始资金（整数）：0=0（默认）。可用范围：0 ~ 无穷大
+    visionModifier            = 0,     -- 全局建筑及部队视野加成（整数）：0=无加成（默认），-1=全部减一，1=全部+1。中立建筑（火焰）不受影响。可用范围：任意整数（无论如何，建筑和部队最低视野为1）
+  },
+
   layers = {
     {
       type = "tilelayer",
